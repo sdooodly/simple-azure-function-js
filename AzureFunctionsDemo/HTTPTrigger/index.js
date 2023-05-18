d7e262ca289866ec7d22d98c4c86bbd9e723f6b7
Gayathri.Sujatha <Gayathri.Sujatha@arup.com>
2023-05-18 11:57:48 +0530

    Initial

---
+++ AzureFunctionsDemo/HTTPTrigger/index.js
@@ -0,0 +1,11 @@
+
+module.exports = (context, req) => {
+    context.log('JavaScript HTTP trigger function processed a request.')
+    context.bindings.res = {
+      body: `The date is ${new Date().toDateString()}<br />The time is ${new Date().toLocaleTimeString()}
+   
+  <p>Query Parameters:</p><pre>${JSON.stringify((req || {}).query, null, ' ')}</pre>`,
+      headers: { 'content-type': 'text/html' }
+    }
+    context.done()
+  }
