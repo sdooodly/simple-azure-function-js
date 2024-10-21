e624be95529021ebc6b42e1c3138c62dbd3596d9
Gayathri Gireesh Sujatha <gayathri.sujatha@arup.com>
2024-10-21 09:58:13 +0530

    kmn

--- AzureFunctionsDemo/HTTPTrigger2/index.js
+++ AzureFunctionsDemo/HTTPTrigger2/index.js
@@ -1,5 +1,6 @@
 
 module.exports = (context, req) => {
+    console.log(req)
     context.log('JavaScript HTTP trigger function processed a request.')
     context.bindings.res = {
       body: `The date is ${new Date().toDateString()}<br />The time is ${new Date().toLocaleTimeString()}
